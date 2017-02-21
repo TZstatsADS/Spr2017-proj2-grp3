05b574f70821fd8b733863f8cc75c5ce61df09cf
MarieSlz <marie.c.schiltz@gmail.com>
2017-02-21 10:16:58 -0500

    Cleaning Tables

--- doc/cleaning_consumption_data.R
+++ doc/cleaning_consumption_data.R
@@ -1,5 +1,10 @@
 
 
-df1 <- read.csv("../data/Behavioral_Risk_Factor_Data__Tobacco_Use__2010_And_Prior_.csv")
-df2 <- read.csv("../data/Behavioral_Risk_Factor_Data__Tobacco_Use__2011_to_present_.csv")
-  
+df1 <- read.csv("./data/Behavioral_Risk_Factor_Data__Tobacco_Use__2010_And_Prior_.csv")
+df2 <- read.csv("./data/Behavioral_Risk_Factor_Data__Tobacco_Use__2011_to_present_.csv")
+df2$DisplayOrder <- NULL
+df2$SubMeasureID <- NULL
+
+df$TopicType <- NULL
+
+df <- rbind(df1,df2)
